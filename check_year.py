859ba15098dcb55605c0de5832d52f36007cd92a
IurieChi <chigaiiura@yahoo.com>
2023-04-18 09:10:02 +0200

    new functinality added to check year

--- check_year.py
+++ check_year.py
@@ -51,6 +51,46 @@ def test():
 		else:
 			print("Failed")
 	
+def days_in_month(year, month):
+    if year < 1582 or month < 1 or month > 12:
+        return None
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    res  = days[month - 1]
+    if month == 2 and is_year_leap(year):
+        res = 29
+    return res
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [ 2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+def month():
+	for i in range(len(test_years)):
+		yr = test_years[i]
+		mo = test_months[i]
+		print(yr,mo,"-> ",end="")
+		result = days_in_month(yr, mo)
+		if result == test_results[i]:
+			print(test_results[i])
+		else:
+			print("Failed")
+
+def day_of_year(year, month, day):
+    #function takes three arguments (a year, a month, and a day of the month) and returns the corresponding day of 
+	# the year, or returns None if any of the arguments is invalid.
+    days = 0
+    for m in range(1, month):
+        md = days_in_month(year, m)
+        if md == None:
+            return None
+        days += md
+    md = days_in_month(year, month)
+    if day >= 1 and day <= md:
+        return days + day
+    else:
+        return None
+
+print(f'Day number {day_of_year(1987, 5, 31)}')
 
 # year_type()
-test()
+# test()
+month()
